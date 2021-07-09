d5f9ab8f70986a88a40ae391054c5bf087f8a6c4
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-09 11:50:06 +0200

    Complete styling of footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,12 +54,62 @@
 
         <footer id="site-footer">
             <div class="container">
-                footer menu
+                <div class="menus">
+                    <ul class="menu">
+                        <li><h2 class="menu-heading">DC Comics</h2></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="/comics">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+    
+                        <li>
+                            <ul class="menu">
+                                <li><h2 class="menu-heading">Shop</h2></li>
+                                <li><a href="#">Shop DC</a></li>
+                                <li><a href="#">Shop DC Colletibles</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+    
+                    <ul class="menu">
+                        <li><h2 class="menu-heading">DC</h2></li>
+                        <li><a href="#">Terms of Use</a></li>
+                        <li><a href="#">Privacy policy (New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+    
+                    <ul class="menu">
+                        <li><h2 class="menu-heading">Sites</h2></li>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
 
+                <div class="disclaimer">
+                    <div>
+                        All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved.
+                    </div>
+
+                    <a href="#">Cookies Settings</a>
+                </div>
             </div>
             <div class="call-to-action">
                 <div class="container">
-                    <button>SIGN UP NOW!</button>
+                    <button>SIGN-UP NOW!</button>
 
                     <div class="social">
                         <span>FOLLOW US</span>
