3477d768f7765185c109736836090e84c6ee1d75
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-09 13:07:28 +0200

    Add blue bar with features in every page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,6 +50,35 @@
         
         <main id="main-content">
             @yield("content")
+
+            <div class="features">
+                <div class="container">
+                    <a href="#">
+                        <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="">
+                        <div>Digital Comics</div>
+                    </a>
+
+                    <a href="#">
+                        <img src="{{ asset('img/buy-comics-merchandise.png') }}" alt="">
+                        <div>DC Merchandise</div>
+                    </a>
+
+                    <a href="#">
+                        <img src="{{ asset('img/buy-comics-subscriptions.png') }}" alt="">
+                        <div>Subscription</div>
+                    </a>
+
+                    <a href="#">
+                        <img src="{{ asset('img/buy-comics-shop-locator.png') }}" alt="">
+                        <div>Comic Shop Locator</div>        
+                    </a>
+
+                    <a href="#">
+                        <img src="{{ asset('img/buy-dc-power-visa.svg') }}" alt="">
+                        <div>DC Power Visa</div>        
+                    </a>
+                </div>
+            </div>
         </main>
 
         <footer id="site-footer">
