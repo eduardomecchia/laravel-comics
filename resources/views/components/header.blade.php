58943f6792d628ea742245744a640cf01aef7d64
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-10 17:44:14 +0200

    Split layout into three components and include them in app.blade.php

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,34 @@
+<!-- Website header -->
+<header id="site-header">
+    <div class="header-top">
+        <div class="container">
+            <a href="#">DC POWER&trade; VISA&reg;</a>
+            <a href="#">ADDITIONAL DC SITES</a>
+        </div>
+    </div>
+
+    <div class="container">
+        <a href="/" class="logo">
+            <img src="{{ asset('img/dc-logo.png') }}" alt="">
+        </a>
+
+        <nav class="header-menu">
+            <a href="#">Characters</a>
+            <a href="/comics">Comics</a>
+            <a href="#">Movies</a>
+            <a href="#">Tv</a>
+            <a href="#">Games</a>
+            <a href="#">Collectibles</a>
+            <a href="#">Videos</a>
+            <a href="#">Fans</a>
+            <a href="#">News</a>
+            <a href="#">Shop</a>
+
+            <div class="searchbox">
+                <input type="text" placeholder="Search"> 
+            </div>
+        </nav>
+    </div>
+
+    <div class="jumbotron"></div>
+</header>
