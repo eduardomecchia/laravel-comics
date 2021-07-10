c188106af674e1c435a7b293bcd4555932ef9e54
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-10 03:44:59 +0200

    Complete second section of individual comic page

--- resources/views/comics/comic.blade.php
+++ resources/views/comics/comic.blade.php
@@ -47,6 +47,68 @@
 
     <!-- Talent and specs of the selected comic -->
     <section class="comic-details">
+        <div class="container details">
+            <div class="talent">
+                <h4>Talent</h4>
 
+                <div class="art">
+                    <div class="label">Art by:</div>
+
+                    <ul>
+                        @foreach($comic["artists"] as $artist)
+                            @if(!$loop->last)
+                                <li>
+                                    <a href="#">{{ $artist }},&nbsp;</a>
+                                </li>
+                            @else
+                                <li>
+                                    <a href="#">{{ $artist }}</a>
+                                </li>    
+                            @endif
+                        @endforeach
+                    </ul>
+                </div>
+
+                <div class="writers">
+                    <div class="label">Written by:</div>
+
+                    <ul>
+                        @foreach($comic["writers"] as $writer)
+                            @if(!$loop->last)
+                                <li>
+                                    <a href="#">{{ $writer }},&nbsp;</a>
+                                </li>
+                            @else
+                                <li>
+                                    <a href="#">{{ $writer }}</a>
+                                </li>    
+                            @endif
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+
+            <div class="specs">
+                <h4>Specs</h4>
+
+                <div class="series">
+                    <div class="label">Series:</div>
+
+                    <a href="#">{{ $comic["series"] }}</a>
+                </div>
+
+                <div class="price-tag">
+                    <div class="label">U.S. Price:</div>
+
+                    <div>{{ $comic["price"] }}</div>
+                </div>
+
+                <div class="sale-date">
+                    <div class="label">On Sale Date:</div>
+
+                    <div>{{ $comic["sale_date"] }}</div>
+                </div>
+            </div>
+        </div>
     </section>
 @endsection
